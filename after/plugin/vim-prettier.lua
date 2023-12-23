9ce1bfde3ba439c19333c881cdc5d3454c98fd62
methias <35785003+tmns@users.noreply.github.com>
2023-12-23 15:10:02 +0800

    Disable Prettier quickfix results

--- after/plugin/vim-prettier.lua
+++ after/plugin/vim-prettier.lua
@@ -3,4 +3,6 @@ vim.keymap.set("n", "<leader>p", "<cmd>PrettierAsync<cr>", { desc = "Format code
 vim.g["prettier#autoformat"] = 1
 vim.g["prettier#autoformat_require_pragma"] = 0
 
+vim.g["prettier#quickfix_enabled"] = 0
+
 vim.g["prettier#exec_cmd_async"] = 1
