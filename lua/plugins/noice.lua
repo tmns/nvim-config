3df8610f82399777e1922b502fb7249b1278cc58
tmns <tmn5@pm.me>
2024-06-09 00:33:43 +0800

    Suppress unwanted msgs

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -22,6 +22,18 @@ return {
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
+			-- Suppress "No information available" messages.
+			routes = {
+				{
+					filter = {
+						event = "notify",
+						find = "No information available",
+					},
+					opts = {
+						skip = true,
+					},
+				},
+			},
 		})
 	end,
 }
